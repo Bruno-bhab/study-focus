376638e88bda01edcc5fa4f2f73497294f80e1f8
bruno.bhab <bruno.bhab@gmail.com>
2023-06-27 22:11:47 -0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <RouterView />
+</template>
+
+<script>
+import { RouterView } from 'vue-router';
+
+export default{
+  name: 'App',
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
